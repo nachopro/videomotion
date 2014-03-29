e909740a6bcb585420e0b1086ee9c743eb7db75c
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2014-03-29 01:26:21 -0300

    Concepto básico

---
+++ src/test.py
@@ -0,0 +1,31 @@
+import tornado.ioloop
+import tornado.web
+
+from uvc import list_devices
+
+
+class HomeHandler(tornado.web.RequestHandler):
+    def get(self):
+        cameras = list_devices()
+        data = {
+            'count': len(cameras),
+            'devices': [{
+                    'name': c['name'],
+                    'device': c['device'],
+                    'url': '/device/%s' % c['device']
+                } for c in cameras]
+        }
+        self.write(data)
+
+class CameraHandler(tornado.web.RequestHandler):
+    def get(self, device):
+        self.write({'device': device})
+
+application = tornado.web.Application([
+    (r"/", HomeHandler),
+    (r"/device/(.*)", CameraHandler),
+], autoreload=True)
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ src/uvc.py
@@ -0,0 +1,7 @@
+
+def list_devices():
+    return [
+            {'name': 'Genius 200c', 'device': 'video0'},
+            {'name': 'Sony Eye', 'device': 'video1'},
+            {'name': 'Canon EDS', 'device': 'video2'},
+        ]
