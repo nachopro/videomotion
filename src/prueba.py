6e4ab3e5c7e3da35ce5877c8317db4f7d45e5063
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2014-04-01 15:31:55 -0300

    Prueba de concepto

--- src/prueba.py
+++ src/prueba.py
@@ -6,27 +6,68 @@
 import v4l2
 import fcntl
 
-vd = open('/dev/video0', 'rw')
 
-cp = v4l2.v4l2_capability()
-fcntl.ioctl(vd, v4l2.VIDIOC_QUERYCAP, cp)
+class WebCam(object):
+    def __init__(self, *args, **kwargs):
+        self._camara = '/dev/video0'
+        self._fd = None
 
-print cp.driver
-print cp.card
-print cp.bus_info
-print cp.version
-print cp.capabilities
-print cp.reserved
+    def _abrir(self):
+        self._fd = open(self._camara, 'rw')
 
-fmt = v4l2.v4l2_format()
-fmt.type = v4l2.V4L2_BUF_TYPE_VIDEO_CAPTURE
-fmt.fmt.pix.width = 1024
-fmt.fmt.pix.height = 768
-fmt.fmt.pix.pixelformat = v4l2.V4L2_PIX_FMT_MJPEG
-fmt.fmt.pix.field = v4l2.V4L2_FIELD_NONE
+    def _cerrar(self):
+        if isinstance(self._fd, file):
+            self._fd.close()
 
-fcntl.ioctl(vd, v4l2.VIDIOC_TRY_FMT, fmt)
+    def camaras(self):
+        pass
 
-print fmt.fmt.pix.width
-print fmt.fmt.pix.height
+    def controles(self):
+        queryctrl = v4l2.v4l2_queryctrl(v4l2.V4L2_CID_BASE)
 
+        while queryctrl.id < v4l2.V4L2_CID_LASTP1:
+            try:
+                ioctl(self._fd, v4l2.VIDIOC_QUERYCTRL, queryctrl)
+            except IOError, e:
+                assert e.errno == errno.EINVAL
+                queryctrl.id += 1
+                continue
+            yield queryctrl
+            queryctrl = v4l2.v4l2_queryctrl(queryctrl.id + 1)
+
+        queryctrl.id = v4l2.V4L2_CID_PRIVATE_BASE
+        while True:
+            try:
+                ioctl(self._fd, v4l2.VIDIOC_QUERYCTRL, queryctrl)
+            except IOError, e:
+                assert e.errno == errno.EINVAL
+                break
+            yield queryctrl
+            queryctrl = v4l2.v4l2_queryctrl(queryctrl.id + 1)
+
+
+#
+#vd = open('/dev/video0', 'rw')
+#
+#cp = v4l2.v4l2_capability()
+#fcntl.ioctl(vd, v4l2.VIDIOC_QUERYCAP, cp)
+#
+#print cp.driver
+#print cp.card
+#print cp.bus_info
+#print cp.version
+#print cp.capabilities
+#print cp.reserved
+#
+#fmt = v4l2.v4l2_format()
+#fmt.type = v4l2.V4L2_BUF_TYPE_VIDEO_CAPTURE
+#fmt.fmt.pix.width = 1024
+#fmt.fmt.pix.height = 768
+#fmt.fmt.pix.pixelformat = v4l2.V4L2_PIX_FMT_MJPEG
+#fmt.fmt.pix.field = v4l2.V4L2_FIELD_NONE
+#
+#fcntl.ioctl(vd, v4l2.VIDIOC_TRY_FMT, fmt)
+#
+#print fmt.fmt.pix.width
+#print fmt.fmt.pix.height
+#
